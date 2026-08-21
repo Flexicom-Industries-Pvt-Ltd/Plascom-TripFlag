2e451ad9e88805191c75dec66e13258cb9b681a3
Debsmit16 <ghosh.debsmit1611@gmail.com>
2026-08-21 13:39:41 +0530

    chore: Remove tracked scratch files and default Next.js assets to clean repository

--- test2.mjs
+++
@@ -1,7 +0,0 @@
-process.env.GROQ_API_KEY = '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';
-
-import('./lib/groq.js').then(async (m) => {
-  console.log('Testing Groq API...');
-  const res = await m.parseNaturalLanguageRule('flag if fuel is above 50');
-  console.log('Result:', res);
-});
